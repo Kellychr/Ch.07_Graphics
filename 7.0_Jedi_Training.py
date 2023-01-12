3c8420f494649d3763b1d0fdabbcdc39b6c0efb1
kellychr <kellychr@urbandaleschools.com>
2023-01-11 18:02:00 -0600

    Ch. 02 Jedi Training

--- 7.0_Jedi_Training.py
+++ 7.0_Jedi_Training.py
@@ -1,7 +1,34 @@
+
+import arcade
 #Sign your name:________________
+
 
 '''
 Recreate, exactly the Test Picture from the website. The arcade colors used in this picture in no particular order are:
 BLACK, ALMOND, PHLOX, BLUSH, RED, BLUE, WISTERIA, AMBER, BRICK_RED and YELLOW.
 The picture is 500px wide and 400px tall. Look up ARC in the documentation to do the PAC-MAN.
 '''
+arcade.open_window(500, 400, "My Drawing", )
+
+arcade.set_background_color(arcade.color.ALMOND)
+arcade.start_render()
+
+# y=50
+# x=10
+for i in range(0,510,20):
+    arcade.draw_line(i, 0, i, 500, (0, 0, 0), 2)
+for h in range(0, 410, 20):
+    arcade.draw_line(0, h, 510, h, (0, 0, 0), 2)
+
+arcade.draw_circle_filled(250,200,40, arcade.color.WISTERIA)
+arcade.draw_text("I love you. I know.",20,160,arcade.color.BRICK_RED,20)
+arcade.draw_rectangle_filled(50,370,60,20,arcade.color.PHLOX,0)
+arcade.draw_arc_filled(400,320,120,120,arcade.color.YELLOW,30,330)
+arcade.draw_ellipse_filled(100,100,120,40,arcade.color.AMBER,0)
+arcade.draw_line(80,20,120,60,arcade.color.BLUE)
+arcade.draw_rectangle_filled(200,260,40,20,arcade.color.BLUSH,-45)
+arcade.draw_point(460,10,arcade.color.RED,6)
+
+arcade.finish_render()
+
+arcade.run()
--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -10,3 +10,52 @@ We will have a competition to see who can make this flag in the least lines of c
 The record is 16! You will have to use some loops to achieve this.
 CHALLENGE: Can you make the entire flag parametrically? This means if I change the hoist to 520px the flag will resize accordingly.
 '''
+
+
+import arcade
+#variable
+y = 260
+x = y * 1.9
+stripe = 0.0769 * y
+v = 10
+m = 40
+c = y * 0.5385
+d = y * 0.76
+#make window
+arcade.open_window(x, y, "The Stars and Stripes")
+#set background color to white
+arcade.set_background_color(arcade.color.WHITE)
+# start window rendering
+arcade.start_render()
+#make 13 stripes
+for i in range(v, y+1, m):
+    arcade.draw_line(0, i, x, i, (191, 10, 48), stripe)
+#make blue rectangle
+arcade.draw_lrtb_rectangle_filled(0, d, y, y - c, (0, 40, 104))
+for i in range(7, 10, 10):
+    arcade.draw_text("*", y-10, i, arcade.color.WHITE, font_size=12)
+arcade.finish_render()
+arcade.run()
+#variable
+y = 260
+x = y * 1.9
+stripe = 0.0769 * y
+v = 10
+m = 40
+c = y * 0.5385
+d = y * 0.76
+#make window
+arcade.open_window(x, y, "The Stars and Stripes")
+#set background color to white
+arcade.set_background_color(arcade.color.WHITE)
+# start window rendering
+arcade.start_render()
+#make 13 stripes
+for i in range(v, y+1, m):
+    arcade.draw_line(0, i, x, i, (191, 10, 48), stripe)
+#make blue rectangle
+arcade.draw_lrtb_rectangle_filled(0, d, y, y - c, (0, 40, 104))
+for i in range(7, 10, 10):
+    arcade.draw_text("*", y-10, i, arcade.color.WHITE, font_size=12)
+arcade.finish_render()
+arcade.run()
